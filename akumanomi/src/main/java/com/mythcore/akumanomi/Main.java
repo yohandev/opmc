c80fce33e0b2ec7804502fa33c392e03e00fb2fc
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-05-24 16:13:12 -0700

    initialized Fabric project `fune`

---
+++ akumanomi/src/main/java/com/mythcore/akumanomi/Main.java
@@ -0,0 +1,18 @@
+package com.mythcore.akumanomi;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer
+{
+	/**
+	 * The unique identifier used for this mod, also configured
+	 * in fabric.mod.json among other places.
+	 */
+	public static final String MOD_ID = "akumanomi";
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ fune/src/main/java/com/yohandev/fune/Main.java
@@ -0,0 +1,18 @@
+package com.yohandev.fune;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer
+{
+	/**
+	 * The unique identifier used for this mod, also configured
+	 * in fabric.mod.json among other places.
+	 */
+	public static final String MOD_ID = "fune";
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
