fd5e4daee3773d1ad5be4d24a945b745eea9156b
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-05-25 10:37:25 -0700

    renamed `Main` class to `Mod`

--- akumanomi/src/main/java/com/mythcore/akumanomi/Mod.java
+++ akumanomi/src/main/java/com/mythcore/akumanomi/Mod.java
@@ -2,13 +2,13 @@ package com.mythcore.akumanomi;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Main implements ModInitializer
+public class Mod implements ModInitializer
 {
 	/**
 	 * The unique identifier used for this mod, also configured
 	 * in fabric.mod.json among other places.
 	 */
-	public static final String MOD_ID = "akumanomi";
+	public static final String ID = "akumanomi";
 
 	@Override
 	public void onInitialize()
