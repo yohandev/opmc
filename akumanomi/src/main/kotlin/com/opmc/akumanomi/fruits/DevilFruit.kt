b7ab2a5d8612382d5eb91d66d2e74a35ac0a094a
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-05-27 16:08:03 -0700

    Modified `DevilFruit` abstract class skeleton to be more OOP friendly

--- akumanomi/src/main/kotlin/com/opmc/akumanomi/fruits/DevilFruit.kt
+++ akumanomi/src/main/kotlin/com/opmc/akumanomi/fruits/DevilFruit.kt
@@ -1,5 +1,7 @@
 package com.opmc.akumanomi.fruits
 
+import com.opmc.akumanomi.util.Clicks
+
 /**
  * An abstract Devil Fruit, responsible for registering
  * click-combo listeners of sub-classes
@@ -10,11 +12,33 @@ abstract class DevilFruit
 	 * Get this Devil Fruit's name in English
 	 * @return Name, including " Fruit" suffix.
 	 */
-	abstract fun nameEn(): String
+	abstract val nameEn: String
 
 	/**
 	 * Get this Devil Fruit's name in Japanese
 	 * @return Name, including " no Mi" suffix.
 	 */
-	abstract fun nameJp(): String
+	abstract val nameJp: String
+
+	/**
+	 * Map of ability bound to each left/right click combination, if any.
+	 */
+	abstract val abilities: Map<Clicks, Ability>
+
+	/** Interface for Devil Fruit abilities */
+	interface Ability
+	{
+		/** Get this ability's name, in English **/
+		val nameEn: String
+		/** Get this ability's name, in Japanese */
+		val nameJp: String
+
+		/** The minimum devil fruit level(inclusive) at which this ability unlocks */
+		val minLvl: Int
+
+		/**
+		 * Execute this ability given the individual clicks that triggered it
+		 */
+		fun execute(/* TODO */)
+	}
 }
--- akumanomi/src/main/kotlin/com/opmc/akumanomi/fruits/FlameFlameFruit.kt
+++ akumanomi/src/main/kotlin/com/opmc/akumanomi/fruits/FlameFlameFruit.kt
@@ -7,35 +7,80 @@ import com.opmc.akumanomi.util.Clicks
  */
 class FlameFlameFruit : DevilFruit()
 {
-	override fun nameEn() = "Flame-Flame Fruit"
-	override fun nameJp() = "Mera Mera no Mi"
+	override val nameEn = "Flame-Flame Fruit"
+	override val nameJp = "Mera Mera no Mi"
+
+	override val abilities = mapOf(
+		Clicks.RRR to LogiaForm,
+		Clicks.RLL to FireFist,
+		Clicks.RLR to BlazeNet,
+		Clicks.RRL to Fireflies,
+	)
+
+	/**
+	 * Renders the user intangible to physical attacks, exceptions being their elements'
+	 * natural weaknesses and Armament Haki
+	 */
+	object LogiaForm : Ability
+	{
+		override val nameEn = "Logia Form"
+		override val nameJp = "Logia Form"
+
+		override val minLvl = 0
+
+		override fun execute()
+		{
+			TODO("Not yet implemented")
+		}
+	}
 
 	/**
 	 * Throws a punch that sets whatever is hit ablaze, and causes an explosion
 	 * that sets a small radius on fire. Consumes 2 hunger.
 	 */
-	@FruitAbility(Clicks.RLL, level = 0)
-	fun fireFist()
+	object FireFist : Ability
 	{
+		override val nameEn = "Fire Fist"
+		override val nameJp = "Hiken"
 
+		override val minLvl = 0
+
+		override fun execute()
+		{
+			TODO("Not yet implemented")
+		}
 	}
 
 	/**
 	 * Creates a ring of fire around the user. Consumes 3 hunger.
 	 */
-	@FruitAbility(Clicks.RLR, level = 5)
-	fun blazeNet()
+	object BlazeNet : Ability
 	{
+		override val nameEn = "Blaze Net"
+		override val nameJp = "Enjomo"
 
+		override val minLvl = 0
+
+		override fun execute()
+		{
+			TODO("Not yet implemented")
+		}
 	}
 
 	/**
 	 * Summons a dozen "fireflies"(red XP entities with a large follow radius) that
 	 * sets whatever is hit on fire. Consumes 4 hunger.
 	 */
-	@FruitAbility(Clicks.RRL, level = 10)
-	fun fireflies()
+	object Fireflies : Ability
 	{
+		override val nameEn = "Fireflies"
+		override val nameJp = "Hotarubi"
 
+		override val minLvl = 0
+
+		override fun execute()
+		{
+			TODO("Not yet implemented")
+		}
 	}
 }
